c1c314fff3661d32343086020e858cf2626df35e
Nicholas Cheong <2082739C@student.gla.ac.uk>
2021-11-03 18:40:00 +0000

    Implemented the custom player controller class. Key presses are stored in a variable (current and last key presses) and last key press is updated at a slight delay (settable in the class)

--- Source/SwordPhysicsSystem/SPSPlayerController.cpp
+++ Source/SwordPhysicsSystem/SPSPlayerController.cpp
@@ -6,6 +6,30 @@
 // Debug file
 #include "DebugOutput.h"
 
+#include "RHI.h"
+
+ASPSPlayerController::ASPSPlayerController() {
+
+	// Initialise key variables such that no copying is required and checks are done on a single instance of each key
+	A_FKey = EKeys::A;
+	D_FKey = EKeys::D;
+	W_FKey = EKeys::W;
+	S_FKey = EKeys::S;
+	LShift_FKey = EKeys::LeftShift;
+	empty_FKey = FKey();
+
+	// Set pointers to none
+	lastKeyPressed = &empty_FKey;
+	currentKeyPressed = &empty_FKey;
+
+	// Set input characteristics
+	lastKeyOverwriteDelayValue = 0.1;
+	lastKeyTimeTillOverwrite = lastKeyOverwriteDelayValue;
+};
+
+
+
+
 
 void ASPSPlayerController::PlayerTick(float DeltaTime) {
 	
@@ -14,8 +38,58 @@ void ASPSPlayerController::PlayerTick(float DeltaTime) {
 
 	DebugOutput output = DebugOutput();
 
+	/* Set the last key pressed to current key pressed AFTER A CERTAIN DELAY(lastKeyOverwriteDelayValue) since this is the next tick
+	   and current key was set from last tick */
 
-	output.toHUD(FString("In SPSPlayerController"), 2.f, false);
+	//  Check if the current key is different from the last key first before considering calculating the delay
+	
 
+	// If the time has come to overwrite the lastKeyPressed
+	if (lastKeyTimeTillOverwrite <= 0) {
+
+		// Delay over, set last key to current key
+		lastKeyPressed = &*(currentKeyPressed);
+
+		// Reset the time to delay 
+		lastKeyTimeTillOverwrite = lastKeyOverwriteDelayValue;
+	}
+	else {
+		// Reduce the time till overwrite by the time difference between this and last frame. 
+		lastKeyTimeTillOverwrite -= DeltaTime;
+	}
+		
+
+	//output.toHUD(FString(lastKeyPressed->GetFName().ToString()), 2.f, false);
+	
+
+
+	/* Setting currentKeyPressed based on user input*/
+	// Set current key by checking if any of the key keys (lol) is pressed
+	if (IsInputKeyDown(EKeys::A)) {
+		currentKeyPressed = &A_FKey;
+	}
+	else if (IsInputKeyDown(EKeys::D)) {
+		currentKeyPressed = &D_FKey;
+	}
+	else if (IsInputKeyDown(EKeys::W)) {
+		currentKeyPressed = &W_FKey;
+	}
+	else if (IsInputKeyDown(EKeys::S)) {
+		currentKeyPressed = &S_FKey;
+	}
+	else if (IsInputKeyDown(EKeys::LeftShift)) {
+		currentKeyPressed = &LShift_FKey;
+	}
+	else {
+		currentKeyPressed = &empty_FKey;
+	}
 }
 
+
+const FKey* ASPSPlayerController::getLastKeyPressed() {
+	return lastKeyPressed;
+}
+
+const FKey* ASPSPlayerController::getCurrentKeyPressed() {
+	return currentKeyPressed;
+}
--- Source/SwordPhysicsSystem/SPSPlayerController.h
+++ Source/SwordPhysicsSystem/SPSPlayerController.h
@@ -10,6 +10,7 @@
 
 /**
  *  This class is set in the BP_GameModeSPS. It automaticalled assigns this as the PlayerController (I think)
+	
 	Purpose of this class is to add functionality to the PlayerController, mainly to be able to
 	store key presses such that inputs can be more flexible (e.g. press A then shift to dodge) 
  */
@@ -19,19 +20,57 @@ class SWORDPHYSICSSYSTEM_API ASPSPlayerController : public APlayerController
 	GENERATED_BODY()
 	
 private:
-	TArray<EKeys> keysPressed; 
+
+
+	// Instances of keys being monitored such that only one instance is requried
+	FKey	empty_FKey;
+	FKey	A_FKey; 
+	FKey	D_FKey;
+	FKey	W_FKey;
+	FKey	S_FKey;
+	FKey	LShift_FKey; 
+
+	// Key variable pointers which switch between FKey instances (no copying required)
+	FKey* lastKeyPressed;
+	FKey* currentKeyPressed;
+
+
+	// Key "sensitivity" to change
+	float lastKeyOverwriteDelayValue; 
+	float lastKeyTimeTillOverwrite;
 
 public:
 
-	// Constructors. Note, super constructor required to be called 
-	ASPSPlayerController() {};
-	ASPSPlayerController(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer) {}
+	// Constructors. Note, super constructor required to be called in order to set up the controller. 
+	ASPSPlayerController();
+	ASPSPlayerController(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer) {
 
+		// Initialise key variables such that no copying is required and checks are done on a single instance of each key
+		A_FKey = EKeys::A;
+		D_FKey = EKeys::D; 
+		W_FKey = EKeys::W;
+		S_FKey = EKeys::S;
+		LShift_FKey = EKeys::LeftShift;
+		empty_FKey = FKey(); 
+
+		// Set pointers to none
+		lastKeyPressed = &empty_FKey;
+		currentKeyPressed = &empty_FKey;
+
+		// Set input characteristics
+		lastKeyOverwriteDelayValue = 0.1;
+		lastKeyTimeTillOverwrite = lastKeyOverwriteDelayValue;
+	}
 
 	/**
 	* Processes player input (immediately after PlayerInput gets ticked) and calls UpdateRotation().
 	* PlayerTick is only called if the PlayerController has a PlayerInput object. Therefore, it will only be called for locally controlled PlayerControllers.
+	
+	This function will be used to store the last and current key pressed. Only certain keys will be checked for. 
 	*/
 	virtual void PlayerTick(float DeltaTime) override;
+
+	const FKey* getLastKeyPressed();
+	const FKey* getCurrentKeyPressed();
 
 };
