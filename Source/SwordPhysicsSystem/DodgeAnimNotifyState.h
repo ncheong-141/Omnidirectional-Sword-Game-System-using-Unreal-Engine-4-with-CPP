48ce67ce401e65e0956ba30b1f3ff88d3c44bf9f
Nicholas Cheong <2082739C@student.gla.ac.uk>
2021-11-24 17:44:16 +0000

    Added A dodge notification state class. This extends the UAnimNotificationState class and allows custom notif state classes to be set on animations. With this, C++ parameters such as isInDodge etc can be set at Begin() and End()of the notification state. The state also has a tick function which may be useful. The class is also used to pass current Animation data to the AnimInstance class in which is used to extract precise float curve values at any moment of time despite blending

---
+++ Source/SwordPhysicsSystem/DodgeAnimNotifyState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "DodgeAnimNotifyState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SWORDPHYSICSSYSTEM_API UDodgeAnimNotifyState : public UAnimNotifyState
+{
+	GENERATED_BODY()
+	
+};
