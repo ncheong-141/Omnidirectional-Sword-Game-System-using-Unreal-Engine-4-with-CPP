6bde737610de4edbccce8a2ee8b28369eee26ace
Nicholas Cheong <2082739C@student.gla.ac.uk>
2021-11-25 18:17:10 +0000

    Added new Anim notification states

---
+++ Source/SwordPhysicsSystem/NPC_StaggeredAnimNotifyState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "NPC_StaggeredAnimNotifyState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SWORDPHYSICSSYSTEM_API UNPC_StaggeredAnimNotifyState : public UAnimNotifyState
+{
+	GENERATED_BODY()
+	
+};
