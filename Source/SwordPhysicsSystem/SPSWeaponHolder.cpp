f8bb33f53ef467ad2875116f2342ac74248ca810
Nicholas Cheong <2082739C@student.gla.ac.uk>
2021-12-14 10:22:56 +0000

    Renamed SPTargetable interface to SPSWeaponHolder as makes much more sense

---
+++ Source/SwordPhysicsSystem/SPSWeaponHolder.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SPSWeaponHolder.h"
+
+// Add default functionality here for any ISPSWeaponHolder functions that are not pure virtual.
